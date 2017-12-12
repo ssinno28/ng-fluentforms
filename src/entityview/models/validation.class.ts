1cf10fd6bd6090a16014f5aec052f78e69287071
ssinno28 <sammi.sinno@gmail.com>
2017-12-12 08:14:37 -0500

    updates to validators

--- src/entityview/models/validation.class.ts
+++ src/entityview/models/validation.class.ts
@@ -3,5 +3,4 @@ import {Validator, Validators} from '@angular/forms';
 export class Validation {
   validator: Validators;
   message: string;
-  arguments: any[];
 }
--- src/entityview/models/validations.class.ts
+++ src/entityview/models/validations.class.ts
@@ -16,8 +16,7 @@ export class Validations {
   min(message: string, min: number): void {
     const validation = new Validation();
     validation.message = message;
-    validation.validator = Validators.min;
-    validation.arguments = [min];
+    validation.validator = Validators.min(min);
 
     this.validations.push(validation);
   }
@@ -25,8 +24,7 @@ export class Validations {
   max(message: string, max: number): void {
     const validation = new Validation();
     validation.message = message;
-    validation.validator = Validators.max;
-    validation.arguments = [max];
+    validation.validator = Validators.max(max);
 
     this.validations.push(validation);
   }
@@ -35,6 +33,30 @@ export class Validations {
     const validation = new Validation();
     validation.message = message;
     validation.validator = CustomValidators.isNumeric;
+
+    this.validations.push(validation);
+  }
+
+  alphanum(message: string): void {
+    const validation = new Validation();
+    validation.message = message;
+    validation.validator = Validators.pattern('^[a-zA-Z0-9]+$');
+
+    this.validations.push(validation);
+  }
+
+  alpha(message: string): void {
+    const validation = new Validation();
+    validation.message = message;
+    validation.validator = Validators.pattern('^[a-zA-Z]+$');
+
+    this.validations.push(validation);
+  }
+
+  email(message: string): void {
+    const validation = new Validation();
+    validation.message = message;
+    validation.validator = Validators.pattern('^((([a-z]|\\d|[!#\\$%&\'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\\.([a-z]|\\d|[!#\\$%&\'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\\d|-|\\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\\d|-|\\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\\.?$');
 
     this.validations.push(validation);
   }
