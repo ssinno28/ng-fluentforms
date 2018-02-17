2933266d4dd6d18162f524a0f3d26019a1faab45
ssinno28 <sammi.sinno@gmail.com>
2018-02-17 10:49:33 -0500

    adding select item as export

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,4 +1,6 @@
 export {Editor} from './fluentforms/editors/base.editor';
 export {BaseFormComponent} from './fluentforms/components/baseform/base.form.component';
 export {BaseFieldComponent} from './fluentforms/components/basefield/basefield.component';
+export {SelectItem} from './fluentforms/interfaces/selectitem.interface';
+
 export * from './fluentforms/fluentforms.module';
