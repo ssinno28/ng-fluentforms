7dcbccf62dce38b20bdb7d33dd2eaae9a7e416b1
ssinno28 <sammi.sinno@gmail.com>
2018-09-01 15:57:37 -0400

    upgrading to newest version of angular

--- src/app/fluentforms/custom-validators.ts
+++ src/app/fluentforms/custom-validators.ts
@@ -1,5 +1,5 @@
 import {AbstractControl} from '@angular/forms';
-import {isNumeric} from 'rxjs/util/isNumeric';
+import {isNumeric} from 'rxjs/internal-compatibility';
 
 export function NumericFn(control: AbstractControl): { [s: string]: boolean } {
   if (!isNumeric(control.value)) {
